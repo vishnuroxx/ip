3eda0a5b1a4b2eee041999696c87b8007fa8d479
Vishnu Kumar <srivishnurajkumar7@gmail.com>
2026-08-16 11:47:13 +0800

    Level 2

--- src/main/java/Hu9o.java
+++ src/main/java/Hu9o.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  * A simple command-line chatbot that echoes user commands.
  */
 public class Hu9o {
+
+    // Task Manager
+    public static ArrayList<String> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         String banner = " _   _           ___ \n"
                 + "| | | | | | | | / _ \\   ___  \n"
                 + "| |_| | | | | || (_) | / _ \\ \n"
                 + "|  _  | | |_| | \\__,| | (_) |\n"
                 + "|_| |_|  \\___/   /_/   \\___/ \n";
+
+        // ChatBot Conversation
         System.out.println("_________________________________");
         System.out.print(banner);
         System.out.println("_________________________________\n");
@@ -39,9 +46,22 @@ public class Hu9o {
      *
      * @param command the command entered by the user
      */
-    private static void answerHandler(String command) { 
-        System.out.println("_________________________________");
-        System.out.println("\n" + command);
+    private static void answerHandler(String command) {
         System.out.println("_________________________________\n");
+        switch (command.toLowerCase()) {
+            case "list":
+                int index = 1;
+                for (String task : tasks) {
+                    System.out.println(index + "." + " " + task);
+                    index++;
+                }
+                break;
+            default:
+                tasks.add(command);
+                System.out.println("added: " + command);
+                break;
+        }
+        System.out.println("_________________________________\n");
+
     }
 }

37c8db354d264bdf5e9b0325d5a40a038b37cbeb
Vishnu Kumar <srivishnurajkumar7@gmail.com>
2026-08-16 10:34:28 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Hu9o.java
@@ -0,0 +1,16 @@
+public class Hu9o {
+    public static void main(String[] args) {
+        String banner = " _   _           ___ \n"
+                + "| | | | | | | | / _ \\   ___  \n"
+                + "| |_| | | | | || (_) | / _ \\ \n"
+                + "|  _  | | |_| | \\__,| | (_) |\n"
+                + "|_| |_|  \\___/   /_/   \\___/ \n";
+        System.out.println("_________________________________");
+        System.out.print(banner);
+        System.out.println("_________________________________\n");
+        System.out.print("Woof! I'm Hu9o!\nWhat can I do for you? \n"); 
+        System.out.println("_________________________________\n");
+        System.out.print("Bye. Hope to see you again soon! (wags tail)\n");
+        System.out.println("_________________________________\n");
+    }
+}

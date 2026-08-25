cf6061a8b1a7be3cde2267584cbc6af735878172
vishnuroxx <183851233+vishnuroxx@users.noreply.github.com>
2026-08-25 16:58:42 +0800

    Added Datetime for deadline and event classes

---
+++ src/main/java/DateTime.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
+
+public class DateTime {
+    private static DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+            .parseCaseInsensitive()
+            .appendPattern("dd/MM/yy h[mm] a")
+            .toFormatter(Locale.ENGLISH);
+
+    private DateTime() {
+    }
+
+    public static LocalDateTime parseString(String str) throws DateTimeParseException {
+        return LocalDateTime.parse(str, formatter);
+    }
+
+    public static String dateToString(LocalDateTime obj) {
+        String toStringFormat = obj.getMinute() > 0 ? "dd/MM/yy hmm a" : "dd/MM/yy h a";
+        return obj.format(DateTimeFormatter.ofPattern(toStringFormat, Locale.US));
+    }
+
+    public static String dateToDisplay(LocalDateTime obj) {
+        String toStringFormat = obj.getMinute() > 0 ? "dd MMM, hmm a" : "dd MMM, h a";
+        return obj.format(DateTimeFormatter.ofPattern(toStringFormat, Locale.US));
+    }
+
+}
--- src/main/java/DeadlineTask.java
+++ src/main/java/DeadlineTask.java
@@ -1,27 +1,31 @@
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
+
 /**
  * Represents a task that has a deadline.
  */
 public class DeadlineTask extends Task {
-    private final String specification;
-
+    private final LocalDateTime specification;
     /**
      * Creates an unfinished deadline task.
      *
      * @param description   the task's description
      * @param specification the deadline, such as "Sunday"
      */
-    public DeadlineTask(String description, String specification) {
+
+    public DeadlineTask(String description, String specification) throws DateTimeParseException {
         super(description, "D");
-        this.specification = specification;
+        this.specification = DateTime.parseString(specification);
     }
 
     @Override
     public String compressionString() {
-        return super.compressionString()+ specification + "|";
+        return super.compressionString() + DateTime.dateToString(specification) + "|";
     }
 
     @Override
     protected String specificationSuffix() {
-        return " (by: " + specification + ")";
+        return " (by: " + DateTime.dateToDisplay(specification) + ")";
     }
 }
